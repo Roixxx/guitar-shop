cd1b33352c3ec7e0584014d0c595f0ed77619992
Roi <vosroix@gmail.com>
2020-10-01 22:15:00 +0500

    some fixies and edited price

--- js/modules/Products.js
+++ js/modules/Products.js
@@ -1,9 +1,9 @@
-import catalog from '/js/modules/catalog.js';
+import {catalog} from '/js/modules/catalog.js';
+
 
 const productsSection = document.getElementById('products');
 
 export default class Products {
-
 
     render() {
         let htmlCatalog = '';
@@ -13,7 +13,7 @@ export default class Products {
                 <li class="products-element">
                     <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${img}">
-                    <span class="products-element__price">${price}</span>
+                    <span class="products-element__price">${price} ₽</span>
                     <button class="products-element__btn">В корзину</button>
                 </li>
             `;  
